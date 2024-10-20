0f2c82b0ba961e1a1d06eed150995b375dc857a7
Иван <cvb@sdf>
2024-10-20 22:00:58 +0300

    RA-06 Внес необходимые правки

--- app/src/main/java/aps/fithom/startandroidapp/data/local/recipes.kt
+++ app/src/main/java/aps/fithom/startandroidapp/data/local/recipes.kt
@@ -3,44 +3,44 @@ package aps.fithom.startandroidapp.data.local
 import kotlinx.serialization.json.Json
 
 object STUB {
-    private val recipesAsString = "[\n" +
-            "    {\n" +
-            "        \"id\": 0,\n" +
-            "        \"title\": \"Бургеры\",\n" +
-            "        \"description\": \"Рецепты всех популярных видов бургеров\",\n" +
-            "        \"imageUrl\": \"burger.png\"\n" +
-            "    },\n" +
-            "    {\n" +
-            "        \"id\": 1,\n" +
-            "        \"title\": \"Десерты\",\n" +
-            "        \"description\": \"Самые вкусные рецепты десертов специально для вас\",\n" +
-            "        \"imageUrl\": \"dessert.png\"\n" +
-            "    },\n" +
-            "    {\n" +
-            "        \"id\": 2,\n" +
-            "        \"title\": \"Пицца\",\n" +
-            "        \"description\": \"Пицца на любой вкус и цвет. Лучшая подборка для тебя\",\n" +
-            "        \"imageUrl\": \"pizza.png\"\n" +
-            "    },\n" +
-            "    {\n" +
-            "        \"id\": 3,\n" +
-            "        \"title\": \"Рыба\",\n" +
-            "        \"description\": \"Печеная, жареная, сушеная, любая рыба на твой вкус\",\n" +
-            "        \"imageUrl\": \"fish.png\"\n" +
-            "    },\n" +
-            "    {\n" +
-            "        \"id\": 4,\n" +
-            "        \"title\": \"Супы\",\n" +
-            "        \"description\": \"От классики до экзотики: мир в одной тарелке\",\n" +
-            "        \"imageUrl\": \"soup.png\"\n" +
-            "    },\n" +
-            "    {\n" +
-            "        \"id\": 5,\n" +
-            "        \"title\": \"Салаты\",\n" +
-            "        \"description\": \"Хрустящий калейдоскоп под соусом вдохновения\",\n" +
-            "        \"imageUrl\": \"salad.png\"\n" +
-            "    }\n" +
-            "]"
+    private val recipesAsString = """[
+    {
+        "id": 0,
+        "title": "Бургеры",
+        "description": "Рецепты всех популярных видов бургеров",
+        "imageUrl": "burger.png"
+    },
+    {
+        "id": 1,
+        "title": "Десерты",
+        "description": "Самые вкусные рецепты десертов специально для вас",
+        "imageUrl": "dessert.png"
+    },
+    {
+        "id": 2,
+        "title": "Пицца",
+        "description": "Пицца на любой вкус и цвет. Лучшая подборка для тебя",
+        "imageUrl": "pizza.png"
+    },
+    {
+        "id": 3,
+        "title": "Рыба",
+        "description": "Печеная, жареная, сушеная, любая рыба на твой вкус",
+        "imageUrl": "fish.png"
+    },
+    {
+        "id": 4,
+        "title": "Супы",
+        "description": "От классики до экзотики: мир в одной тарелке",
+        "imageUrl": "soup.png"
+    },
+    {
+        "id": 5,
+        "title": "Салаты",
+        "description": "Хрустящий калейдоскоп под соусом вдохновения",
+        "imageUrl": "salad.png"
+    }
+]"""
     private val categories = Json.decodeFromString<List<Category>>(recipesAsString)
     fun getCategories(): List<Category> = categories
 }
