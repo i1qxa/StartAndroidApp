971872ab0f55edc459f3917c162e4eeba09ab9ac
ivan <kuiv1989@gmail.com>
2025-02-18 14:35:27 +0300

    Вынес создание Dispatcher в конструктор класса RecipesRepository

--- app/src/main/java/aps/fithom/startandroidapp/data/remote/RecipesRepository.kt
+++ app/src/main/java/aps/fithom/startandroidapp/data/remote/RecipesRepository.kt
@@ -2,16 +2,17 @@ package aps.fithom.startandroidapp.data.remote
 
 import aps.fithom.startandroidapp.domain.models.Category
 import aps.fithom.startandroidapp.domain.models.Recipe
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.withContext
 
-class RecipesRepository {
+class RecipesRepository(private val defaultDispatcher: CoroutineDispatcher = Dispatchers.IO) {
 
     private val recipeService = RetrofitClient.instance
 
-    suspend fun getCategories(): Deferred<List<Category>?> = withContext(Dispatchers.IO) {
+    suspend fun getCategories(): Deferred<List<Category>?> = withContext(defaultDispatcher) {
         async {
             recipeService.getCategories().let { call ->
                 call.execute().let { response ->
@@ -26,7 +27,7 @@ class RecipesRepository {
     }
 
     suspend fun getRecipesByCategoryId(categoryId: Int): Deferred<List<Recipe>?> =
-        withContext(Dispatchers.IO) {
+        withContext(defaultDispatcher) {
             async {
                 recipeService.getRecipesByCategoryId(categoryId).let { call ->
                     call.execute().let { response ->
@@ -41,7 +42,7 @@ class RecipesRepository {
         }
 
     suspend fun getRecipesByIds(ids: Set<Int>): Deferred<List<Recipe>?> =
-        withContext(Dispatchers.IO) {
+        withContext(defaultDispatcher) {
             async {
                 recipeService.getRecipesByIds(ids.joinToString(separator = ",")).let { call ->
                     call.execute().let { response ->
@@ -55,7 +56,7 @@ class RecipesRepository {
             }
         }
 
-    suspend fun getRecipeById(recipeId: Int): Deferred<Recipe?> = withContext(Dispatchers.IO) {
+    suspend fun getRecipeById(recipeId: Int): Deferred<Recipe?> = withContext(defaultDispatcher) {
         async {
             recipeService.getRecipeById(recipeId).let { call ->
                 call.execute().let { response ->
