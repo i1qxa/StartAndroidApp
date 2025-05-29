e801e8824b7a9a1126c50917208c8701d1f9835c
Иван <cvb@sdf>
2025-05-29 20:47:51 +0300

    Убрал лишний код, перенёс создание БД в RecipeModule

--- app/src/main/java/aps/fithom/startandroidapp/data/di/RecipeModule.kt
+++ app/src/main/java/aps/fithom/startandroidapp/data/di/RecipeModule.kt
@@ -1,6 +1,7 @@
 package aps.fithom.startandroidapp.data.di
 
-import android.app.Application
+import android.content.Context
+import androidx.room.Room
 import aps.fithom.startandroidapp.data.local.db.CategoryDao
 import aps.fithom.startandroidapp.data.local.db.RecipeDao
 import aps.fithom.startandroidapp.data.local.db.RecipesDataBase
@@ -9,31 +10,32 @@ import aps.fithom.startandroidapp.data.remote.RetrofitClient
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 class RecipeModule {
 
-//    @Provides
-//    fun provideApplication(): Application =
-//        HiltAndroidApp::class.java.getDeclaredField("application").let { field ->
-//            field.isAccessible = true
-//            field.get(null) as RecipesApplication
-//        }
-
+    @Singleton
     @Provides
-    fun provideDatabase(application: Application): RecipesDataBase =
-        RecipesDataBase.getInstance(application)
+    fun provideDatabase(@ApplicationContext context: Context): RecipesDataBase =
+        Room.databaseBuilder(
+            context,
+            RecipesDataBase::class.java,
+            "recipes_db"
+        ).build()
 
+    @Singleton
     @Provides
     fun provideCategoryDao(recipeDB: RecipesDataBase): CategoryDao = recipeDB.categoryDao()
 
+    @Singleton
     @Provides
     fun provideRecipeDao(recipeDB: RecipesDataBase): RecipeDao = recipeDB.recipeDao()
 
     @Provides
     fun provideRecipesService(): RecipesService = RetrofitClient.instance
-
 
 }
--- app/src/main/java/aps/fithom/startandroidapp/data/local/db/RecipesDataBase.kt
+++ app/src/main/java/aps/fithom/startandroidapp/data/local/db/RecipesDataBase.kt
@@ -1,8 +1,6 @@
 package aps.fithom.startandroidapp.data.local.db
 
-import android.app.Application
 import androidx.room.Database
-import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import aps.fithom.startandroidapp.domain.models.Category
@@ -21,28 +19,4 @@ abstract class RecipesDataBase : RoomDatabase() {
     abstract fun categoryDao(): CategoryDao
     abstract fun recipeDao(): RecipeDao
 
-    companion object {
-        private var INSTANCE: RecipesDataBase? = null
-        private val LOCK = Any()
-        private const val DB_NAME = "recipes_db"
-
-        fun getInstance(application: Application): RecipesDataBase {
-            INSTANCE?.let {
-                return it
-            }
-            synchronized(LOCK) {
-                INSTANCE?.let {
-                    return it
-                }
-                val db = Room.databaseBuilder(
-                    application,
-                    RecipesDataBase::class.java,
-                    DB_NAME
-                )
-                    .build()
-                INSTANCE = db
-                return db
-            }
-        }
-    }
 }
